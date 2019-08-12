05122040034c60c2e85c06f87b7e7813e6323e8d
Pete McLean <ptmclean@gmail.com>
2019-08-12 21:47:08 +1000

    Add the ability to set the name, description and excluded characters in a secret. (#3)

--- test/test.js
+++ test/test.js
@@ -21,3 +21,33 @@ test.serial('with-kms', async t => {
     t.pass(); 
   }
 });
+test.serial('with-name', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/with-name.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass(); 
+  }
+});
+test.serial('with-description', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/with-description.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass(); 
+  }
+});
+test.serial('with-excluded-chars', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/with-excluded-chars.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass(); 
+  }
+});
